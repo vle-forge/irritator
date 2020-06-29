5fcdeb8998fe209d0409e1100cac8b5c43f39c5f
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2020-06-29 23:43:19 +0200

    core: change event on cross model
    cmakelist: minor
    
    benchmark: adding tests

--- lib/include/irritator/core.hpp
+++ lib/include/irritator/core.hpp
@@ -3301,7 +3301,7 @@ struct integrator
 
         if(up_threshold < val) 
         {
-            return reset_value;
+            return up_threshold;
         }
         else if (down_threshold > val)
         {
@@ -4664,6 +4664,7 @@ struct cross
     {
         bool have_message = false;
         bool have_message_value = false;
+        event = 0.0; 
 
        for (const auto& msg : input_ports.get(x[port_threshold]).messages) {
             irt_return_if_fail(msg.type == value_type::real_64,
