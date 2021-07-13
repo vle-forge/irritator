1233bb86050fadbb4472d130e524fc6824d64f93
ese ovie <oveseovese@gmail.com>
2021-07-13 04:56:47 +0100

    commit node on master again

--- app/gui/node-editor.cpp
+++ app/gui/node-editor.cpp
@@ -1663,12 +1663,6 @@ show_dynamics_values(simulation& /*sim*/, const counter& dyn)
 }
 
 static void
-show_dynamics_values(simulation& /*sim*/, const filter& dyn)
-{
-    ImGui::Text("input %.3f", dyn.x);
-}
-
-static void
 show_dynamics_values(simulation& /*sim*/, const queue& dyn)
 {
     if (dyn.queue.empty()) {
@@ -1794,6 +1788,12 @@ show_dynamics_values(simulation& /*sim*/, const time_func& dyn)
 static void
 show_dynamics_inputs(editor& /*ed*/, none& /*dyn*/)
 {}
+
+static void
+show_dynamics_values(simulation& /*sim*/, const filter& dyn)
+{
+    ImGui::Text("input %.3f", dyn.x);
+}
 
 static void
 show_dynamics_values(simulation& /*sim*/, const flow& dyn)
@@ -3088,6 +3088,21 @@ editor::show_window() noexcept
                       3,
                       "Fail to initialize example_qss_izhikevich<1>: %s\n",
                       status_string(ret));
+            if (ImGui::MenuItem("Insert example QSS1 seir_lineaire")) 
+                if (auto ret = example_qss_seir_lineaire<1>(sim, empty_fun);
+                    is_bad(ret))
+                    log_w.log(
+                      3,
+                      "Fail to initialize example_qss_seir_lineaire<1>: %s\n",
+                      status_string(ret));
+            if (ImGui::MenuItem("Insert example QSS1 seir_nonlineaire"))
+                if (auto ret = example_qss_seir_nonlineaire<1>(sim, empty_fun);
+                    is_bad(ret))
+                    log_w.log(
+                        3,
+                        "Fail to initialize "
+                        "example_qss_seir_nonlineaire<1>: %s\n",
+                        status_string(ret));
 
             if (ImGui::MenuItem("Insert example QSS2 lotka_volterra"))
                 if (auto ret = example_qss_lotka_volterra<2>(sim, empty_fun);
@@ -3121,13 +3136,6 @@ editor::show_window() noexcept
                     log_w.log(
                       3,
                       "Fail to initialize example_qss_izhikevich<2>: %s\n",
-                      status_string(ret));
-            if (ImGui::MenuItem("Insert example QSS2 seir_nonlinear"))
-                if (auto ret = example_qss_seir_nonlinear<2>(sim, empty_fun);
-                    is_bad(ret))
-                    log_w.log(
-                      3,
-                      "Fail to initialize example_qss_seir_nonlinear<2>: %s\n",
                       status_string(ret));
 
             if (ImGui::MenuItem("Insert example QSS3 lotka_volterra"))
