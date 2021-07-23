b832299cde643859420eabca2b0367d1ec410f6a
ese ovie <oveseovese@gmail.com>
2021-07-23 02:02:25 +0100

    model: added qss1 on SEIR model

--- app/gui/node-editor.cpp
+++ app/gui/node-editor.cpp
@@ -3088,6 +3088,13 @@ editor::show_window() noexcept
                       3,
                       "Fail to initialize example_qss_izhikevich<1>: %s\n",
                       status_string(ret));
+            if (ImGui::MenuItem("Insert example QSS1 seir_nonlinear"))
+                if (auto ret = example_qss_seir_nonlinear<1>(sim, empty_fun);
+                    is_bad(ret))
+                    log_w.log(
+                      3,
+                      "Fail to initialize example_qss_seir_nonlinear<1>: %s\n",
+                      status_string(ret));
 
             if (ImGui::MenuItem("Insert example QSS2 lotka_volterra"))
                 if (auto ret = example_qss_lotka_volterra<2>(sim, empty_fun);
@@ -3125,10 +3132,10 @@ editor::show_window() noexcept
             if (ImGui::MenuItem("Insert example QSS2 seir_nonlinear"))
                 if (auto ret = example_qss_seir_nonlinear<2>(sim, empty_fun);
                     is_bad(ret))
-                    log_w.log(3,
-                              "Fail to initialize "
-                              "example_qss_seir_nonlinear<2>: %s\n",
-                              status_string(ret));
+                    log_w.log(
+                      3,
+                      "Fail to initialize example_qss_seir_nonlinear<2>: %s\n",
+                      status_string(ret));
 
             if (ImGui::MenuItem("Insert example QSS3 lotka_volterra"))
                 if (auto ret = example_qss_lotka_volterra<3>(sim, empty_fun);
