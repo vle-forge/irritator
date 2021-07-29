378fba3cfdd07a3f3581f00059d58fb025ed18ff
ese ovie <oveseovese@gmail.com>
2021-07-29 14:03:47 +0100

    model: SEIR fixed core.hpp issues

--- lib/include/irritator/core.hpp
+++ lib/include/irritator/core.hpp
@@ -6023,6 +6023,7 @@ max_size_in_bytes() noexcept
                sizeof(cross),
                sizeof(time_func),
                sizeof(accumulator_2),
+               sizeof(filter),
                sizeof(flow));
 }
 
@@ -6269,6 +6270,8 @@ dynamics_typeof() noexcept
         return dynamics_type::time_func;
     if constexpr (std::is_same_v<Dynamics, accumulator_2>)
         return dynamics_type::accumulator_2;
+    if constexpr (std::is_same_v<Dynamics, filter>)
+        return dynamics_type::filter;
     if constexpr (std::is_same_v<Dynamics, flow>)
         return dynamics_type::flow;
 
@@ -6448,6 +6451,8 @@ struct simulation
             return f(*reinterpret_cast<accumulator_2*>(&mdl.dyn), args...);
         case dynamics_type::time_func:
             return f(*reinterpret_cast<time_func*>(&mdl.dyn), args...);
+        case dynamics_type::filter:
+            return f(*reinterpret_cast<filter*>(&mdl.dyn), args...);
         case dynamics_type::flow:
             return f(*reinterpret_cast<flow*>(&mdl.dyn), args...);
         }
@@ -6576,6 +6581,8 @@ struct simulation
                      args...);
         case dynamics_type::time_func:
             return f(*reinterpret_cast<const time_func*>(&mdl.dyn), args...);
+        case dynamics_type::filter:
+            return f(*reinterpret_cast<const filter*>(&mdl.dyn), args...);
         case dynamics_type::flow:
             return f(*reinterpret_cast<const flow*>(&mdl.dyn), args...);
         }
@@ -6879,6 +6886,7 @@ public:
         case dynamics_type::constant:
         case dynamics_type::cross:
         case dynamics_type::time_func:
+        case dynamics_type::filter:
         case dynamics_type::flow:
         case dynamics_type::accumulator_2:
             if (mdl_dst.type == dynamics_type::integrator &&
