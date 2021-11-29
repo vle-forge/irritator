b70b5b1cb13aee9b0cb9b9bba3a90499cb1718e7
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2021-11-29 11:42:15 +0100

    gui: replace simulation window with tab

--- app/gui/simulation-window.cpp
+++ app/gui/simulation-window.cpp
@@ -9,59 +9,72 @@
 
 namespace irt {
 
+static void show_simulation(component_editor& ed) noexcept
+{
+    ImGui::InputReal("Begin", &ed.simulation_begin);
+    ImGui::InputReal("End", &ed.simulation_end);
+    ImGui::TextFormat("Current time {:.6f}", ed.simulation_begin);
+
+    bool start_line = false;
+    if (match(ed.simulation_state,
+              component_simulation_status::not_started,
+              component_simulation_status::finished)) {
+        start_line = true;
+        if (ImGui::Button("init")) {
+            ed.simulation_init();
+        }
+    }
+
+    if (ed.simulation_state == component_simulation_status::initialized) {
+        if (start_line)
+            ImGui::SameLine();
+        else
+            start_line = true;
+
+        if (ImGui::Button("start")) {
+            ed.simulation_start();
+        }
+    }
+
+    if (ed.simulation_state == component_simulation_status::pause_forced) {
+        if (start_line)
+            ImGui::SameLine();
+        else
+            start_line = true;
+
+        if (ImGui::Button("continue")) {
+            ed.simulation_start();
+        }
+    }
+
+    if (ed.simulation_state == component_simulation_status::running) {
+        if (start_line)
+            ImGui::SameLine();
+
+        if (ImGui::Button("pause")) {
+            ed.force_pause = true;
+        }
+
+        ImGui::SameLine();
+        if (ImGui::Button("stop")) {
+            ed.force_stop = true;
+        }
+    }
+}
+
 void component_editor::show_simulation_window() noexcept
 {
-    if (ImGui::CollapsingHeader("Simulation##Component",
-                                ImGuiTreeNodeFlags_CollapsingHeader)) {
-        ImGui::InputReal("Begin", &simulation_begin);
-        ImGui::InputReal("End", &simulation_end);
-        ImGui::TextFormat("Current time {:.6f}", simulation_begin);
-
-        bool start_line = false;
-        if (match(simulation_state,
-                  component_simulation_status::not_started,
-                  component_simulation_status::finished)) {
-            start_line = true;
-            if (ImGui::Button("init")) {
-                simulation_init();
-            }
+    if (ImGui::BeginTabBar("##Simulation")) {
+        if (ImGui::BeginTabItem("Simulation")) {
+            show_simulation(*this);
+            ImGui::EndTabItem();
         }
 
-        if (simulation_state == component_simulation_status::initialized) {
-            if (start_line)
-                ImGui::SameLine();
-            else
-                start_line = true;
-
-            if (ImGui::Button("start")) {
-                simulation_start();
-            }
+        if (ImGui::BeginTabItem("Data")) {
+            ImGui::EndTabItem();
         }
 
-        if (simulation_state == component_simulation_status::pause_forced) {
-            if (start_line)
-                ImGui::SameLine();
-            else
-                start_line = true;
-
-            if (ImGui::Button("continue")) {
-                simulation_start();
-            }
-        }
-
-        if (simulation_state == component_simulation_status::running) {
-            if (start_line)
-                ImGui::SameLine();
-
-            if (ImGui::Button("pause")) {
-                force_pause = true;
-            }
-
-            ImGui::SameLine();
-            if (ImGui::Button("stop")) {
-                force_stop = true;
-            }
-        }
+        ImGui::EndTabBar();
     }
 }
 
