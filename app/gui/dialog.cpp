7bb37d43e527c28c7ae71b380246d1e0ae140c19
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2021-12-15 21:46:27 +0100

    gui: fix macos utf8 string use

--- app/gui/dialog.cpp
+++ app/gui/dialog.cpp
@@ -262,14 +262,8 @@ struct file_dialog
     std::vector<std::filesystem::path> paths;
     std::filesystem::path              current;
     std::filesystem::path              selected;
-#if defined(__APPLE__)
-    // @TODO Remove this part when XCode allows u8string concatenation.
-    std::string temp;
-    char        buffer[512];
-#else
     std::u8string temp;
-    char8_t       buffer[512];
-#endif
+    char          buffer[512];
 
     bool is_open = true;
 
@@ -307,22 +301,12 @@ struct file_dialog
 
         const char8_t** filters = file_filters;
 
-#if defined(__APPLE__)
-        // @TODO Remove this part when XCode allows u8string find/compare.
-        while (*filters) {
-            if (p.u8string().find(reinterpret_cast<const char*>(*filters)) == 0)
-                return true;
-
-            ++filters;
-        }
-#else
         while (*filters) {
             if (p.u8string().find(*filters) == 0)
                 return true;
 
             ++filters;
         }
-#endif
 
         return false;
     }
@@ -334,23 +318,12 @@ struct file_dialog
 
         const char8_t** filters = extension_filters;
 
-#if defined(__APPLE__)
-        // @TODO Remove this part when XCode allows u8string find/compare.
-        while (*filters) {
-            if (p.extension().u8string() ==
-                reinterpret_cast<const char*>(*filters))
-                return true;
-
-            ++filters;
-        }
-#else
         while (*filters) {
             if (p.extension().u8string() == *filters)
                 return true;
 
             ++filters;
         }
-#endif
 
         return false;
     }
@@ -509,16 +482,8 @@ bool load_file_dialog(std::filesystem::path& out,
                 fd.temp.clear();
                 std::error_code ec;
                 if (std::filesystem::is_directory(*it, ec)) {
-#if defined(__APPLE__)
-                    // @TODO Remove this part when XCode allows u8string
-                    // concatenation.
-                    fd.temp = "[Dir] ";
-                    fd.temp += reinterpret_cast<const char*>(
-                      it->filename().u8string().c_str());
-#else
                     fd.temp = u8"[Dir] ";
                     fd.temp += it->filename().u8string();
-#endif
                 } else
                     fd.temp = it->filename().u8string();
 
@@ -632,13 +597,7 @@ bool save_file_dialog(std::filesystem::path& out,
                 fd.temp.clear();
                 std::error_code ec;
                 if (std::filesystem::is_directory(*it, ec))
-#if defined(__APPLE__)
-                    // @TODO Remove this part when XCode allows u8string
-                    // concatenation.
-                    fd.temp = "[Dir] ";
-#else
                     fd.temp = u8"[Dir] ";
-#endif
 
                 fd.temp = it->filename().u8string();
 
@@ -756,16 +715,8 @@ bool select_directory_dialog(std::filesystem::path& out)
                 fd.temp.clear();
                 std::error_code ec;
                 if (std::filesystem::is_directory(*it, ec)) {
-#if defined(__APPLE__)
-                    // @TODO Remove this part when XCode allows u8string
-                    // concatenation.
-                    fd.temp = "[Dir] ";
-                    fd.temp += reinterpret_cast<const char*>(
-                      it->filename().u8string().c_str());
-#else
                     fd.temp = u8"[Dir] ";
                     fd.temp += it->filename().u8string();
-#endif
 
                     if (ImGui::Selectable((const char*)fd.temp.c_str(),
                                           (it->filename() == fd.selected))) {
