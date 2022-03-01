6ee4a1aa69c536879ad9ac83878ddc999b454507
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2022-03-01 10:16:56 +0100

    cmake: add a imconfig.h file

---
+++ app/gui/imconfig.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 INRA Distributed under the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef ORG_VLEPROJECT_IRRITATOR_APP_IMCONFIG_H
+#define ORG_VLEPROJECT_IRRITATOR_APP_IMCONFIG_H
+
+//-----------------------------------------------------------------------------
+// COMPILE-TIME OPTIONS FOR DEAR IMGUI
+//-----------------------------------------------------------------------------
+
+// #define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+#define IMGUI_DISABLE_OBSOLETE_KEYIO
+
+#endif
