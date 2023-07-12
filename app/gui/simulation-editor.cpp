c1727bda4e1e56effe34ddda4fb39023c29e1fae
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2023-07-12 10:05:01 +0200

    mod: add a new helpers file with mod algorithms

--- app/gui/simulation-editor.cpp
+++ app/gui/simulation-editor.cpp
@@ -3,13 +3,18 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <optional>
+
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include <imgui.h>
 #include <imgui_internal.h>
 
-#include "irritator/helpers.hpp"
-#include "irritator/modeling.hpp"
-#include "irritator/observation.hpp"
+#include <irritator/core.hpp>
+#include <irritator/helpers.hpp>
+#include <irritator/io.hpp>
+#include <irritator/modeling-helpers.hpp>
+#include <irritator/modeling.hpp>
+#include <irritator/observation.hpp>
+#include <irritator/timeline.hpp>
 
 #ifdef _WIN32
 #define NOMINMAX
@@ -31,106 +36,7 @@
 #include <fmt/format.h>
 #include <fmt/ostream.h>
 
-#include <irritator/core.hpp>
-#include <irritator/io.hpp>
-#include <irritator/timeline.hpp>
-
 namespace irt {
-
-template<typename Function>
-void for_each_child(modeling&          mod,
-                    component&         compo,
-                    generic_component& generic,
-                    Function&&         f) noexcept
-{
-    for_each_data(mod.children, generic.children, [&](auto& child) noexcept {
-        f(compo, generic, child);
-    });
-}
-
-template<typename Function>
-void for_each_child(modeling&       mod,
-                    component&      compo,
-                    grid_component& grid,
-                    Function&&      f) noexcept
-{
-    for_each_data(mod.children, grid.cache, [&](auto& child) noexcept {
-        f(compo, grid, child);
-    });
-}
-
-template<typename Function>
-void for_each_child(modeling&        mod,
-                    component&       compo,
-                    graph_component& graph,
-                    Function&&       f) noexcept
-{
-    for_each_data(mod.children, graph.children, [&](auto& child) noexcept {
-        f(compo, graph, child);
-    });
-}
-
-template<typename Function>
-void for_each_child(modeling& mod, component& compo, Function&& f) noexcept
-{
-    switch (compo.type) {
-    case component_type::simple:
-        if_data_exists_do(mod.simple_components,
-                          compo.id.simple_id,
-                          [&](auto& generic) noexcept {
-                              for_each_child(mod, compo, generic, f);
-                          });
-        break;
-
-    case component_type::internal:
-        break;
-
-    case component_type::grid:
-        if_data_exists_do(
-          mod.grid_components, compo.id.grid_id, [&](auto& grid) noexcept {
-              for_each_child(mod, compo, grid, f);
-          });
-        break;
-
-    case component_type::graph:
-        if_data_exists_do(
-          mod.graph_components, compo.id.graph_id, [&](auto& graph) noexcept {
-              for_each_child(mod, compo, graph, f);
-          });
-        break;
-
-    default:
-        irt_unreachable();
-    }
-}
-
-template<typename Function>
-void for_each_child(modeling& mod, tree_node& tn, Function&& f) noexcept
-{
-    if_data_exists_do(mod.components, tn.id, [&](auto& compo) noexcept {
-        for_each_child(mod, compo, f);
-    });
-}
-
-template<typename Function>
-void for_each_model(simulation& sim, tree_node& tn, Function&& f) noexcept
-{
-    for (int i = 0, e = tn.nodes_v.data.ssize(); i != e; ++i) {
-        switch (tn.nodes_v.data[i].value.index()) {
-        case 0:
-            break;
-
-        case 1: {
-            auto& mdl_id = *std::get_if<model_id>(&tn.nodes_v.data[i].value);
-            if_data_exists_do(
-              sim.models, mdl_id, [&](auto& mdl) { f(sim, tn, mdl); });
-        } break;
-
-        default:
-            irt_unreachable();
-        }
-    }
-}
 
 const char* observable_type_names[] = {
     "none", "file", "plot", "graph", "grid",
@@ -1732,7 +1638,7 @@ static bool show_generic_simulation_settings(application& app,
             ImGui::TableHeadersRow();
 
             for_each_model(
-              app.sim, tn, [&](auto& sim, auto& tn, auto& mdl) noexcept {
+              app.sim, tn, [&](auto& sim, auto& /*tn*/, auto& mdl) noexcept {
                   const auto mdl_id = sim.get_id(mdl);
 
                   ImGui::PushID(get_index(mdl_id));
@@ -2043,7 +1949,7 @@ static void show_select_observation_model(application&            app,
 
         if (ImGui::TreeNodeEx(str.c_str(), flags)) {
             for_each_model(
-              app.sim, tn, [&](auto& sim, auto& tn, auto& mdl) noexcept {
+              app.sim, tn, [&](auto& sim, auto& /*tn*/, auto& mdl) noexcept {
                   const auto mdl_id = sim.models.get_id(mdl);
 
                   str = dynamics_type_names[ordinal(mdl.type)];
---
+++ lib/include/irritator/modeling-helpers.hpp
@@ -0,0 +1,110 @@
+// Copyright (c) 2023 INRA Distributed under the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef ORG_VLEPROJECT_IRRITATOR_2023_MODELING_HELPERS_HPP
+#define ORG_VLEPROJECT_IRRITATOR_2023_MODELING_HELPERS_HPP
+
+#include <irritator/core.hpp>
+#include <irritator/helpers.hpp>
+
+namespace irt {
+
+template<typename Function>
+void for_each_child(modeling&          mod,
+                    component&         compo,
+                    generic_component& generic,
+                    Function&&         f) noexcept
+{
+    for_each_data(mod.children, generic.children, [&](auto& child) noexcept {
+        f(compo, generic, child);
+    });
+}
+
+template<typename Function>
+void for_each_child(modeling&       mod,
+                    component&      compo,
+                    grid_component& grid,
+                    Function&&      f) noexcept
+{
+    for_each_data(mod.children, grid.cache, [&](auto& child) noexcept {
+        f(compo, grid, child);
+    });
+}
+
+template<typename Function>
+void for_each_child(modeling&        mod,
+                    component&       compo,
+                    graph_component& graph,
+                    Function&&       f) noexcept
+{
+    for_each_data(mod.children, graph.children, [&](auto& child) noexcept {
+        f(compo, graph, child);
+    });
+}
+
+template<typename Function>
+void for_each_child(modeling& mod, component& compo, Function&& f) noexcept
+{
+    switch (compo.type) {
+    case component_type::simple:
+        if_data_exists_do(mod.simple_components,
+                          compo.id.simple_id,
+                          [&](auto& generic) noexcept {
+                              for_each_child(mod, compo, generic, f);
+                          });
+        break;
+
+    case component_type::internal:
+        break;
+
+    case component_type::grid:
+        if_data_exists_do(
+          mod.grid_components, compo.id.grid_id, [&](auto& grid) noexcept {
+              for_each_child(mod, compo, grid, f);
+          });
+        break;
+
+    case component_type::graph:
+        if_data_exists_do(
+          mod.graph_components, compo.id.graph_id, [&](auto& graph) noexcept {
+              for_each_child(mod, compo, graph, f);
+          });
+        break;
+
+    default:
+        irt_unreachable();
+    }
+}
+
+template<typename Function>
+void for_each_child(modeling& mod, tree_node& tn, Function&& f) noexcept
+{
+    if_data_exists_do(mod.components, tn.id, [&](auto& compo) noexcept {
+        for_each_child(mod, compo, f);
+    });
+}
+
+template<typename Function>
+void for_each_model(simulation& sim, tree_node& tn, Function&& f) noexcept
+{
+    for (int i = 0, e = tn.nodes_v.data.ssize(); i != e; ++i) {
+        switch (tn.nodes_v.data[i].value.index()) {
+        case 0:
+            break;
+
+        case 1: {
+            auto& mdl_id = *std::get_if<model_id>(&tn.nodes_v.data[i].value);
+            if_data_exists_do(
+              sim.models, mdl_id, [&](auto& mdl) { f(sim, tn, mdl); });
+        } break;
+
+        default:
+            irt_unreachable();
+        }
+    }
+}
+
+} // namespace irt
+
+#endif
