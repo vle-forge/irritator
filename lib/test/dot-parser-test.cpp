aecf9e9eb9048289f0b9d0f9436f866396b407d0
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2024-11-20 16:17:38 +0100

    wip

--- lib/test/dot-parser-test.cpp
+++ lib/test/dot-parser-test.cpp
@@ -11,21 +11,23 @@ enum class edge_id;
 
 struct graph {
     irt::id_array<node_id, irt::default_allocator> nodes;
-    irt::vector<std::string_view>                  node_names;
-    irt::vector<int>                               node_ids;
-    irt::vector<float[2]>                          node_positions;
-    irt::vector<float>                             node_areas;
-
     irt::id_array<edge_id, irt::default_allocator> edges;
-    irt::vector<node_id[2]>                        edges_nodes;
+
+    irt::vector<std::string_view> node_names;
+    irt::vector<int>              node_ids;
+    irt::vector<float[2]>         node_positions;
+    irt::vector<float>            node_areas;
+    irt::vector<node_id[2]>       edges_nodes;
 
     irt::string_buffer buffer;
 
     irt::table<std::string_view, node_id> name_to_node_id;
-    bool                                  sort_before_search = false;
+
+    bool sort_before_search = false;
 };
 
-auto find_or_add = [](graph& g, std::string_view name) noexcept -> node_id {
+static auto find_or_add(graph& g, std::string_view name) noexcept -> node_id
+{
     if (g.sort_before_search)
         g.name_to_node_id.sort();
 
@@ -42,9 +44,9 @@ auto find_or_add = [](graph& g, std::string_view name) noexcept -> node_id {
     g.name_to_node_id.data.emplace_back(g.node_names[idx], id);
 
     return id;
-};
+}
 
-auto to_float(std::string_view str) noexcept -> float
+static auto to_float(std::string_view str) noexcept -> float
 {
     auto f = 0.f;
     if (auto ret = std::from_chars(str.data(), str.data() + str.size(), f);
@@ -54,37 +56,55 @@ auto to_float(std::string_view str) noexcept -> float
     return 0.f;
 }
 
-auto to_2float(std::string_view str) noexcept -> std::pair<float, float>
+static auto to_int(std::string_view str) noexcept -> int
 {
-    float f[2] = { 0.f, 0.f };
-    auto ret = std::from_chars(str.data(), str.data() + str.size(), f[0]);
-    if (ret.ec != std::errc{})
-        return { 0.f, 0.f };
+    int i = 0;
+    if (auto ret = std::from_chars(str.data(), str.data() + str.size(), i);
+        ret.ec != std::errc{})
+        return i;
 
-    const auto length = ret.ptr - str.data();
-    const auto substr = str.substr(length)
-
-    if (auto ret = std::from_chars(ret.ptr, )
-
-    return 0.f;
+    return 0;
 }
 
-auto attach = [](graph&           g,
-                 std::string_view name,
-                 std::span<std::pair<std::string_view, std::string_view>>
-                   attributes) noexcept -> bool {
+static auto to_2float(std::string_view str) noexcept -> std::pair<float, float>
+{
+    float f[2] = { 0.f, 0.f };
+
+    if (const auto ret =
+          std::from_chars(str.data(), str.data() + str.size(), f[0]);
+        ret.ec != std::errc{}) {
+        return { 0.f, 0.f };
+    } else {
+        const auto length = ret.ptr - str.data();
+        const auto substr = str.substr(length);
+
+        if (const auto ret2 = std::from_chars(
+              substr.data(), substr.data() + substr.size(), f[1]);
+            ret2.ec != std::errc{})
+            return { 0.f, 0.f };
+
+        return { f[0], f[1] };
+    }
+}
+
+static auto attach(
+  graph&                                                   g,
+  std::string_view                                         name,
+  std::span<std::pair<std::string_view, std::string_view>> attributes) noexcept
+  -> bool
+{
     const auto id  = find_or_add(g, name);
     const auto idx = irt::get_index(id);
 
     for (const auto& att : attributes) {
         if (att.first == "area")
-            g.node_areas[idx] = irt::to_float(att.second);
+            g.node_areas[idx] = to_float(att.second);
         else if (att.first == "pos")
-            g.node_positions[idx] = irt::to_2float(att.second);
+            g.node_positions[idx] = to_2float(att.second);
         else if (att.first == "id")
-            g.node_ids[idx] = irt::to_int(att.second);
+            g.node_ids[idx] = to_int(att.second);
     }
-};
+}
 
 auto ret = irt::parse_dot_buffer(
   buf,
