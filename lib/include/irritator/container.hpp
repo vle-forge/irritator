4ec73d3134d3be0f65c7606e0e1ddc844d8d6077
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2025-06-23 14:15:05 +0200

    macros: moves integer types from core.hpp to macros.hpp

--- lib/include/irritator/container.hpp
+++ lib/include/irritator/container.hpp
@@ -18,9 +18,6 @@
 #include <type_traits>
 #include <utility>
 
-#include <cstddef>
-#include <cstdint>
-
 namespace irt {
 
 ////////////////////////////////////////////////////////////////////////
@@ -28,19 +25,6 @@ namespace irt {
 // Helpers functions................................................. //
 //                                                                    //
 ////////////////////////////////////////////////////////////////////////
-
-using i8  = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
-using u8  = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
-using sz  = size_t;
-using ssz = ptrdiff_t;
-using f32 = float;
-using f64 = double;
 
 template<typename T>
 concept not_integral = not std::is_integral_v<T>;
--- lib/include/irritator/macros.hpp
+++ lib/include/irritator/macros.hpp
@@ -5,6 +5,8 @@
 #ifndef ORG_VLEPROJECT_IRRITATOR_MACROS_2024
 #define ORG_VLEPROJECT_IRRITATOR_MACROS_2024
 
+#include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 
 #include <fstream>
@@ -17,6 +19,19 @@
 #endif
 
 namespace irt {
+
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using sz  = size_t;
+using ssz = ptrdiff_t;
+using f32 = float;
+using f64 = double;
 
 #ifndef IRRITATOR_REAL_TYPE_F32
 using real = double;
