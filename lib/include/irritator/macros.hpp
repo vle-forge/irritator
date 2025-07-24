28373317a5d4e081bac24856b1d60896314f901a
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2025-07-24 11:20:55 +0200

    macros: adding a debug::breakpoint in unreachable fn

--- lib/include/irritator/macros.hpp
+++ lib/include/irritator/macros.hpp
@@ -142,6 +142,8 @@ void breakpoint() noexcept;
 
 [[noreturn]] inline void unreachable() noexcept
 {
+    ::irt::debug::breakpoint();
+
     // Uses compiler specific extensions if possible.
     // Even if no extension is used, undefined behavior is still raised by
     // an empty function body and the noreturn attribute.
