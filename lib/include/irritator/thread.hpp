fb6e86916de74e52b34f3d89e5fdf1079c516f09
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2025-12-15 23:02:57 +0100

    thread: limits cpu overhead in unordered-task

--- lib/include/irritator/thread.hpp
+++ lib/include/irritator/thread.hpp
@@ -289,7 +289,8 @@ public:
                                     m_lists.end(),
                                     [](const auto& l) { return l.stopping(); }))
                         break;
-                    std::this_thread::yield();
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                    // std::this_thread::yield();
                 }
             }
         });
